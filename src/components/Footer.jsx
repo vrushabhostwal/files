87bf272a53256e597da33a3e4ebb77689e90def4
vrushabhostwal <128209381+vrushabhostwal@users.noreply.github.com>
2026-06-18 00:34:16 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+
+  return (
+    <footer className="bg-maroon text-center py-12">
+
+      <h2 className="font-script text-4xl text-gold">
+        Pranil & Shrushti
+      </h2>
+
+      <p className="text-white mt-4">
+        With Love & Blessings
+      </p>
+
+    </footer>
+  );
+}
