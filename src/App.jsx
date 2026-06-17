028a1fada3628708c6306d881851fd19ff571d1b
vrushabhostwal <128209381+vrushabhostwal@users.noreply.github.com>
2026-06-18 00:35:00 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Hero from "./components/Hero";
+import Countdown from "./components/Countdown";
+import Events from "./components/Events";
+import Venue from "./components/Venue";
+import Gallery from "./components/Gallery";
+import Footer from "./components/Footer";
+
+function App() {
+
+  return (
+    <>
+      <Hero />
+      <Countdown />
+      <Events />
+      <Venue />
+      <Gallery />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
