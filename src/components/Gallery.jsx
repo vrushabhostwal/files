02983d43ca5ca789f2aa53481af7731e488e943d
vrushabhostwal <128209381+vrushabhostwal@users.noreply.github.com>
2026-06-18 00:33:35 +0530

    Create Gallery.jsx

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,16 @@
+export default function Gallery() {
+
+  return (
+    <section className="py-20 text-center">
+
+      <h2 className="text-gold text-6xl font-display">
+        Our Gallery
+      </h2>
+
+      <p className="mt-6">
+        Photos will be added soon
+      </p>
+
+    </section>
+  );
+}
